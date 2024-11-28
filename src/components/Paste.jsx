6c0f5f94524079ac8015376a4ca13fbcf2618adf
Vishal Prajapati <vishalprajapati3266@gmail.com>
2024-11-28 21:08:54 +0530

    phla commit

--- src/components/Paste.jsx
+++ src/components/Paste.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import toast from "react-hot-toast";
+import { useSelector, useDispatch } from "react-redux";
 
 const Paste = () => {
+  const pastes = useSelector((state)=> state.paste.pastes);
+  console.log(pastes);
   return (
     <div>Paste</div>
   )
 }
 
-export default Paste
+export default Paste;
--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -1,5 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { act } from 'react'
+import { createSlice } from '@reduxjs/toolkit';
 import toast from 'react-hot-toast';
 
 const initialState = {
@@ -15,16 +14,29 @@ export const pasteSlice = createSlice({
       const paste = action.payload;
       state.pastes.push(paste);
       localStorage.setItem("pastes", JSON.stringify(state.pastes));
-      toast("Paste Create Successfully")
+      toast.success("Paste Create Successfully")
     },
     updateToPastes: (state, action) => {
-      
+      const paste = action.payload
+      const index = state.pastes.findIndex((item)=> item._id === paste._id)
+      if(index>=0){
+        state.pastes[index] = paste
+        localStorage.setItem("pastes", JSON.stringify(state.pastes));
+        toast.success("Paste Updated")
+      }
     },
     resetAllPastes: (state, action) => {
-      
+      state.pastes = [];
+      localStorage.removeItem("pastes");
     },
     removeFromPastes:(state, action)=>{
-
+        const pasteId = action.payload;
+        const index = state.pastes.findIndex((item)=> item._id === pasteId);
+        if(index >= 0){
+            state.pastes.splice(index,1);
+            localStorage.setItem("pastes", JSON.stringify(state.pastes));
+            toast.success("Paste deleted");
+        }
     },
   },
 })
